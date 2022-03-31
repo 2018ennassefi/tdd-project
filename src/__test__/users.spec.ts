315d23002ef1ee67f12f266d7d2cf5394baf8280
Najib <boutaib.najib1@gmail.com>
2022-03-31 14:52:25 +0200

    Red step : adding tests

--- src/__test__/users.spec.ts
+++ src/__test__/users.spec.ts
@@ -4,6 +4,7 @@ import UserRepository from "src/data/usersRepository/userRepository";
 import UserSQLRepository from "src/data/usersRepository/userSQLRepository";
 import UserDomainService from "src/services/users/userDomainService";
 import { createUsersTable, clearUsersTable } from "src/utils/sql-scripts";
+import { v4 as uuid4 } from "uuid";
 
 let userService: UserDomainService;
 let userRepository: UserRepository;
@@ -26,32 +27,51 @@ describe("Testing user resource", () => {
     await clearUsersTable(db);
   });
 
-  it("should create a new user", async () => {
-    const userId = await userService.createUser("epit");
-
-    expect(userId).toBeTruthy();
+  describe("# Creating users", () => {
+    it("should create a new user", async () => {
+      const userId = await userService.createUser("epit");
+  
+      expect(userId).toBeTruthy();
+    });
   });
 
-  it("should delete an existing user", async () => {
-    const userId = await userService.createUser("iamadeadman");
-    const isDeleted = await userService.removeUser(userId);
-
-    expect(isDeleted).toBe(true);
+  describe("# Deleting users", () => {
+    it("should delete an existing user", async () => {
+      const userId = await userService.createUser("iamadeadman");
+      const isDeleted = await userService.removeUser(userId);
+  
+      expect(isDeleted).toBe(true);
+    });
   });
 
-  it("should get a user by id", async () => {
-    const userId = await userService.createUser("newusername");
-    const user = await userService.searchById(userId);
+  describe("# Searching users", () => {
+    it("should get a user by id", async () => {
+      const userId = await userService.createUser("newusername");
+      const user = await userService.searchById(userId);
+  
+      expect(user.id).toBe(userId);
+      expect(user.username).toBe("newusername");
+    });
 
-    expect(user.id).toBe(userId);
-    expect(user.username).toBe("newusername");
-  });
+    it("should return undefined if user not found by id", async () => {
+      const someId = uuid4();
+      const user = await userService.searchById(someId);
 
-  it("should get a user by pseudoname", async () => {
-    const userId = await userService.createUser("some-pseudo");
-    const user = await userService.searchByPseudo("some-pseudo");
+      expect(user).toBeUndefined();
+    });
+  
+    it("should get a user by pseudoname", async () => {
+      const userId = await userService.createUser("some-pseudo");
+      const user = await userService.searchByPseudo("some-pseudo");
+  
+      expect(user.id).toBe(userId);
+      expect(user.username).toBe("some-pseudo");
+    });
 
-    expect(user.id).toBe(userId);
-    expect(user.username).toBe("some-pseudo");
+    it("should return undefined if user not found by username", async () => {
+      const user = await userService.searchByPseudo("non-existing-name");
+
+      expect(user).toBeUndefined();
+    });
   });
 });
