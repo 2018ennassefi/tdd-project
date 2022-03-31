0609689eb0321d5bc4a16c6333b2d7e45e7b9e75
Najib <boutaib.najib1@gmail.com>
2022-03-31 14:56:09 +0200

    Green step : Implement & Refacto

--- src/services/users/userAbstractService.ts
+++ src/services/users/userAbstractService.ts
@@ -3,8 +3,8 @@ import UserEntity from "src/services/users/userEntity";
 interface UserAbstarctService {
   createUser(name: string): Promise<string>;
   removeUser(id: string): Promise<boolean>;
-  searchById(id: string): Promise<UserEntity>;
-  searchByPseudo(pseudo: string): Promise<UserEntity>;
+  searchById(id: string): Promise<UserEntity | undefined>;
+  searchByPseudo(pseudo: string): Promise<UserEntity | undefined>;
 }
 
 export default UserAbstarctService;
