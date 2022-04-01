2bfb90ff5cb525df7b92b390b26c26a3fc2f5570
Najib <boutaib.najib1@gmail.com>
2022-04-01 10:09:14 +0200

    Configure routing

--- src/routes.ts
+++ src/routes.ts
@@ -1,4 +1,17 @@
 import { Router } from "express";
+import UserController from "src/controllers/users/controller";
+import UserRouter from "src/controllers/users/router";
+import UserSQLRepository from "src/data/usersRepository/userSQLRepository";
+import UserDomainService from "src/services/users/userDomainService";
+import { connectSQLite, createTables } from "src/utils/sqlite-connect";
 
 const baseRouter = Router();
+
+connectSQLite('main-database.sql').then(async (db) => {
+  await createTables(db);
+
+  const c = new UserController(new UserDomainService(new UserSQLRepository(db)));
+  baseRouter.use('/users', new UserRouter(c).build());
+});
+
 export default baseRouter;
--- src/server.ts
+++ src/server.ts
@@ -3,7 +3,7 @@ import createApp from "src/app";
 
 async function launchServer() {
   // Serving Express instance
-  const app = createApp();
+  const app = await createApp();
   const port = Number(process.env.PORT || 5000);
   app.listen(port, () => {
     console.info("Express server started on port: " + port);
